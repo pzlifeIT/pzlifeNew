402d6fa74b521516ac164ac107eaaaecccd142f1
wind-dust <1002380377@qq.com>
2018-12-21 11:01:59 +0800

    test提交

--- application/console/com/User.php
+++ application/console/com/User.php
@@ -4,9 +4,26 @@ namespace app\console\com;
 
 use app\console\Pzlife;
 use Env;
+use think\Db;
+use Config;
 
 class User extends Pzlife {
     public function api($params) {
-        echo $params;
+        
+        $member = "SELECT * FROM pre_member ORDER BY uid ASC LIMIT 1 ";
+        echo '<pre>';
+        var_dump( Config::get('database.db_config') );
+        echo '</pre>';
+        exit;
+        
+        $res = Db::connect(Config::get('database.db_config'))->query($member);
+        
+        while ($value = Db::query($member)) {
+            echo '<pre>';
+            var_dump( $value );
+            echo '</pre>';
+            exit;
+            
+        }
     }
 }
