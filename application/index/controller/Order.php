6a0a65f93850d00da6e48074d89d9dc2f07f548d
zhangyanrong <cyre_zyr@163.com>
2019-01-23 18:18:24 +0800

    fix

--- application/index/controller/Order.php
+++ application/index/controller/Order.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: zhangyanrong
- * Date: 2019/1/23
- * Time: 2:16 PM
- */
+
+namespace app\index\controller;
+
+use app\index\MyController;
+
+class Order extends MyController {
+    protected $beforeActionList = [
+        'isLogin',//所有方法的前置操作
+//        'isLogin' => ['except' => ''],//除去getFirstCate其他方法都进行second前置操作
+//        'three'  => ['only' => 'hello,data'],//只有hello,data方法进行three前置操作
+    ];
+
+
+}
