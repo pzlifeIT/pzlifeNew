ba71f09e6ab7b52c7221c6b149af8ec31852a817
wind-dust <1002380377@qq.com>
2019-01-24 18:19:14 +0800

    登录判断

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -7,7 +7,7 @@ use app\index\MyController;
 class Category extends MyController {
     protected $beforeActionList = [
 //        'isLogin',//所有方法的前置操作
-        'isLogin' => ['except' => 'getFirstCate'],//除去getFirstCate其他方法都进行isLogin前置操作
+        'isLogin' => ['except' => 'getFirstCate,getGoodsSubject'],//除去getFirstCate其他方法都进行isLogin前置操作
 //        'three'  => ['only' => 'hello,data'],//只有hello,data方法进行three前置操作
     ];
 
