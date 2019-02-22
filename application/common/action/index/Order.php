676c9bd75707a51484c708a63a9fce88eb879a71
zhangyanrong <cyre_zyr@163.com>
2019-02-22 14:26:16 +0800

    fix

--- application/common/action/index/Order.php
+++ application/common/action/index/Order.php
@@ -418,7 +418,7 @@ class Order extends CommonIndex {
             }
             /* 运费模版 end */
         }
-        $totalPrice = bcadd($totalGoodsPrice, $totalFreightPrice);
+        $totalPrice = bcadd($totalGoodsPrice, $totalFreightPrice, 2);
         return ['code' => '200', 'goods_count' => $goodsCount, 'rebate_all' => $rebateAll, 'total_goods_price' => $totalGoodsPrice, 'total_freight_price' => $totalFreightPrice, 'total_price' => $totalPrice, 'goods_list' => $goodsList, 'freight_supplier_price' => $freightSupplierPrice];
     }
 
@@ -551,12 +551,12 @@ class Order extends CommonIndex {
             return ['code' => '3000'];
         }
         $field = 'id,order_no,third_order_id,uid,order_status,order_money,deduction_money,pay_money,goods_money,discount_money,deduction_money,third_money';
-        $where  = ['uid' => $uid];
+        $where = ['uid' => $uid];
         if ($order_status) {
             $where = ['uid' => $uid, 'order_status' => $order_status];
         }
         // print_r($order_status);die;
-        
+
         $limit  = $offset . ',' . $pagenum;
         $result = DbOrder::getOrder($field, $where, false, $limit);
         if (empty($result)) {
@@ -565,27 +565,27 @@ class Order extends CommonIndex {
 
         foreach ($result as $key => $value) {
             $order_child = DbOrder::getOrderChild('*', ['order_id' => $value['id']]);
-            
+
             $express_money = 0;
             foreach ($order_child as $order => $child) {
-                $order_goods = DbOrder::getOrderGoods('goods_id,goods_name,order_child_id,sku_id,sup_id,goods_type,goods_price,margin_price,integral,goods_num,sku_json', ['order_child_id' => $child['id']],false,true);
-                $order_goods_num = DbOrder::getOrderGoods('sku_id,COUNT(goods_num) as goods_num', ['order_child_id' => $child['id']],'sku_id');
+                $order_goods     = DbOrder::getOrderGoods('goods_id,goods_name,order_child_id,sku_id,sup_id,goods_type,goods_price,margin_price,integral,goods_num,sku_json', ['order_child_id' => $child['id']], false, true);
+                $order_goods_num = DbOrder::getOrderGoods('sku_id,COUNT(goods_num) as goods_num', ['order_child_id' => $child['id']], 'sku_id');
                 foreach ($order_goods as $og => $goods) {
                     foreach ($order_goods_num as $ogn => $goods_num) {
                         if ($goods_num['sku_id'] == $goods['sku_id']) {
                             $order_goods[$og]['goods_num'] = $goods_num['goods_num'];
-                            $order_goods[$og]['sku_json'] = json_decode($order_goods[$og]['sku_json'],true);
+                            $order_goods[$og]['sku_json']  = json_decode($order_goods[$og]['sku_json'], true);
                         }
                     }
                 }
                 // dump( Db::getLastSql());die;
-                
+
                 $order_child[$order]['order_goods'] = $order_goods;
-               
-                $express_money += $child['express_money'] ;
+
+                $express_money += $child['express_money'];
             }
             $result[$key]['express_money'] = $express_money;
-            $result[$key]['order_child'] = $order_child;
+            $result[$key]['order_child']   = $order_child;
         }
         return ['code' => '200', 'order_list' => $result];
     }
