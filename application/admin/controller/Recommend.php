25e384135e3232b8a223de752889024d135b6264
wind-dust <1002380377@qq.com>
2019-03-06 12:39:59 +0800

    首页完善

--- application/admin/controller/Recommend.php
+++ application/admin/controller/Recommend.php
@@ -7,7 +7,7 @@ use app\admin\AdminController;
 class Recommend extends AdminController
 {
     /**
-     * @api              {post} / 获取推荐信息(倒序排序)【未完成】
+     * @api              {post} / 获取推荐信息【未完成】
      * @apiDescription   getRecommend
      * @apiGroup         admin_Recommend
      * @apiName          getRecommend
@@ -59,8 +59,7 @@ class Recommend extends AdminController
      * @apiParam (入参) {String} show_days 展示每周天数(1:周一，2:周二...,7:周日)
      * @apiParam (入参) {Number} tier 层级
      * @apiParam (入参) {Number} model_order 模板排序
-     * @apiParam (入参) {Number} model_son_order 模板子内容排序
-     * @apiSuccess (返回) {String} code 200:成功 / 3000:用户列表空 / 3001:model_id只能是数字 / 3002:无效的model_id / 3003:title,jump_type,jump_content参数不完整 / 3004:请上传图片 / 3005:未设置显示自然日或者未获取到parent_id / 3006:请设置展示商品 / 3007:未获取到parent_id / 3008:非法参数 / 3009:超出添加数量
+     * @apiSuccess (返回) {String} code 200:成功 / 3000:用户列表空 / 3001:model_id只能是数字 / 3002:无效的model_id / 3003:title,jump_type,jump_content参数不完整 / 3004:请上传图片 / 3005:未设置显示自然日或者未获取到parent_id / 3006:请设置展示商品 / 3007:未获取到parent_id / 3008:非法参数 / 3009:超出添加数量 / 3010:图片没有上传过 / 3011:添加失败
      * @apiSampleRequest /admin/Recommend/addRecommend
      * @apiParamExample (data) {Array} 返回
      * [
@@ -82,7 +81,6 @@ class Recommend extends AdminController
         $model_son_order = trim($this->request->post('model_son_order'));
         $tier            = trim($this->request->post('tier'));
         $model_order     = $model_order ? $model_order : 0;
-        $model_son_order = $model_son_order ? $model_son_order : 0;
         $parent_id       = $parent_id ? $parent_id : 0;
         $tier            = $tier ? $tier : 1;
         $show_type       = $show_type ? $show_type : 1;
@@ -175,7 +173,7 @@ class Recommend extends AdminController
         $data['tier'] = $tier;
         $data['model_order'] = $model_order;
         $data['model_son_order'] = $model_son_order;
-        $result = $this->app->recommend->addRecommend($data);
+        $result = $this->app->recommend->saveRecommend($data);
         return $result;
     }
 
@@ -201,6 +199,195 @@ class Recommend extends AdminController
             return ['code' => '3001'];
         }
         $result = $this->app->recommend->getRecommendId($model_id,$tier);
+        return $result;
+    }
+
+    /**
+     * @api              {post} / 获取推荐详情
+     * @apiDescription   getRecommendInfo
+     * @apiGroup         admin_Recommend
+     * @apiName          getRecommendInfo
+     * @apiParam (入参) {Number} id 推荐ID
+     * @apiSuccess (返回) {String} code 200:成功 / 3000:查询结果为空 / 3001:id只能是数字 
+     * @apiSuccess (recommends_info) {String} id 
+     * @apiSuccess (recommends_info) {String} model_id 模板id 1:轮播banner 2:图标tips 3:专题模块推荐 4:新品上市 5:每周推荐 6:爆款推荐 7:应季推荐 8:时令推荐 9:买主推荐 10:专题商品推荐
+     * @apiSuccess (recommends_info) {String} title 标题
+     * @apiSuccess (recommends_info) {String} image_path 图片路径
+     * @apiSuccess (recommends_info) {String} parent_id 关联上级ID
+     * @apiSuccess (recommends_info) {String} jump_type 跳转类型: 1:专题 2:商品 3:路径
+     * @apiSuccess (recommends_info) {String} jump_content 跳转内容
+     * @apiSuccess (recommends_info) {String} show_type 展示类型: 1:图片 2:商品
+     * @apiSuccess (recommends_info) {String} show_data 展示商品ID
+     * @apiSuccess (recommends_info) {String} show_days 展示每周天数
+     * @apiSuccess (recommends_info) {String} tier 层级
+     * @apiSuccess (recommends_info) {String} model_order 模板排序
+     * @apiSampleRequest /admin/Recommend/getRecommendInfo
+     * @apiParamExample (data) {Array} 返回
+     * [
+     * "code":"200",返回code码
+     * "recommends_info":{
+     * "id": 1,
+     * "model_id": 1,
+     * "title": "banner",
+     * "image_path": "",
+     * "parent_id": 0,
+     * "jump_type": 1,
+     * "jump_content": "",
+     * "show_type": 1,
+     * "show_data": 0,
+     * "show_days": 1,
+     * "tier": 1,
+     * "model_order": 0,
+     * "model_son_order": 0,
+     * "create_time": "2019-03-05 19:20:02",
+     * "update_time": "2019-03-05 19:20:02",
+     * "delete_time": null
+     * }
+     * ]
+     * @author rzc
+     */
+    public function getRecommendInfo(){
+        $id = trim($this->request->post('id'));
+        if (!$id || !is_numeric($id))  {
+            return ['code' => 3001];
+        }
+        $result = $this->app->recommend->getRecommendInfo($id);
+        return $result;
+    }
+
+    /**
+     * @api              {post} / 修改推荐
+     * @apiDescription   updateRecommend
+     * @apiGroup         admin_Recommend
+     * @apiName          updateRecommend
+     * @apiParam (入参) {Number} id 修改内容对应ID
+     * @apiParam (入参) {Number} model_id 模板类型 1:轮播banner 2:图标tips 3:专题模块推荐 4:新品上市 5:每周推荐 6:爆款推荐 7:应季推荐 8:时令推荐 9:买主推荐 10:专题商品推荐
+     * @apiParam (入参) {String} title 标题
+     * @apiParam (入参) {String} image_path 图片路径
+     * @apiParam (入参) {Number} parent_id 关联上级ID
+     * @apiParam (入参) {Number} jump_type 跳转类型: 1:专题 2:商品 3:路径
+     * @apiParam (入参) {String} jump_content 跳转内容
+     * @apiParam (入参) {String} show_type 展示类型: 1:图片 2:商品
+     * @apiParam (入参) {String} show_data 展示商品ID
+     * @apiParam (入参) {String} show_days 展示每周天数(1:周一，2:周二...,7:周日)
+     * @apiParam (入参) {Number} tier 层级
+     * @apiParam (入参) {Number} model_order 模板排序
+     * @apiSuccess (返回) {String} code 200:成功 / 3000:查询结果为空，无法更改 / 3001:model_id和id只能是数字 / 3002:无效的model_id / 3003:title,jump_type,jump_content参数不完整 / 3004:请上传图片 / 3005:未设置显示自然日或者未获取到parent_id / 3006:请设置展示商品 / 3007:未获取到parent_id / 3008:非法参数 / 3010:图片没有上传过 / 3011:修改失败
+     * @apiSampleRequest /admin/Recommend/updateRecommend
+     * @apiParamExample (data) {Array} 返回
+     * [
+     * "code":"200",返回code码
+     * ]
+     * @author rzc
+     */
+    public function updateRecommend(){
+        $id              = trim($this->request->post('id'));
+        $model_id        = trim($this->request->post('model_id'));
+        $title           = trim($this->request->post('title'));
+        $image_path      = trim($this->request->post('image_path'));
+        $parent_id       = trim($this->request->post('parent_id'));
+        $jump_type       = trim($this->request->post('jump_type'));
+        $jump_content    = trim($this->request->post('jump_content'));
+        $show_type       = trim($this->request->post('show_type'));
+        $show_data       = trim($this->request->post('show_data'));
+        $show_days       = trim($this->request->post('show_days'));
+        $model_order     = trim($this->request->post('model_order'));
+        $model_son_order = trim($this->request->post('model_son_order'));
+        $tier            = trim($this->request->post('tier'));
+        $model_order     = $model_order ? $model_order : 0;
+        $parent_id       = $parent_id ? $parent_id : 0;
+        $tier            = $tier ? $tier : 1;
+        $show_type       = $show_type ? $show_type : 1;
+        if (!is_numeric($model_id) && !is_numeric($id)) {
+            return ['code' => '3001'];
+        }
+        $model_arr = [1,2,3,4,5,6,7,8,9,10];
+        if (!in_array($model_id,$model_arr)) {
+            return ['code' => '3002'];
+        }
+        if ($model_id == 1 || $model_id == 2 || $model_id == 3 ) {
+            if ($tier == 2  ) {
+                if (!$title  || !$jump_type || !$jump_content){
+                    return ['code' => '3003'];
+                }
+                if (!$image_path || !$parent_id) {
+                    return ['code' => '3004'];
+                }
+                
+            }
+        }elseif ($model_id == 5) {
+            if (!$title || !$image_path || !$jump_type || !$jump_content){
+                return ['code' => '3003'];
+            }
+            if ($tier == 2 ){
+                if ( !$show_days || !$parent_id) {
+                    return ['code' => '3005'];
+                }
+            }
+        }elseif ($model_id == 6 || $model_id == 4 || $model_id == 7 || $model_id == 8 || $model_id == 9) {
+            if ($tier == 1){
+                if (!$title  || !$jump_type || !$jump_content){
+                    return ['code' => '3003'];
+                }
+                if ($model_id == 7 || $model_id == 8 ) {
+                    if (!$image_path){
+                        return ['code' => '3004'];
+                    }
+                }
+            }elseif ($tier == 2){
+                if (!$parent_id) {
+                    return ['code' => '3007'];
+                }
+                if ($show_type == 1){
+                    if (!$title  || !$jump_type || !$jump_content || !$image_path ){
+                        return ['code' => '3003'];
+                    }
+                }elseif($show_type == 2){
+                    if (!is_numeric($show_data)){
+                        return ['code' => '3006'];
+                    }
+                    if (!$title  || !$jump_type || !$jump_content){
+                        return ['code' => '3003'];
+                    }
+                }
+            }
+        }elseif ($model_id == 10) {
+            if ($tier == 1){
+
+            }elseif ($tier == 2) {
+                if (!$parent_id) {
+                    return ['code' => '3007'];
+                }
+                if (!$title  || !$jump_type || !$jump_content || !$image_path ){
+                    return ['code' => '3003'];
+                }
+            }elseif ($tier == 3) {
+                if ($show_type!=2){
+                    return ['code' => '3008'];
+                }
+                if (!$parent_id) {
+                    return ['code' => '3007'];
+                }
+                if (!is_numeric($show_data)){
+                    return ['code' => '3006'];
+                }
+            }
+        }
+
+        $data = [];
+        $data['model_id'] = $model_id;
+        $data['title'] = $title;
+        $data['image_path'] = $image_path;
+        $data['parent_id'] = $parent_id;
+        $data['jump_type'] = $jump_type;
+        $data['jump_content'] = $jump_content;
+        $data['show_type'] = $show_type;
+        $data['show_data'] = $show_data;
+        $data['show_days'] = $show_days;
+        $data['tier'] = $tier;
+        $data['model_order'] = $model_order;
+        $data['model_son_order'] = $model_son_order;
+        $result = $this->app->recommend->saveRecommend($data,$id);
         return $result;
     }
 }
--- application/common/action/admin/Recommend.php
+++ application/common/action/admin/Recommend.php
@@ -3,9 +3,11 @@ namespace app\common\action\admin;
 
 use app\facade\DbRecommend;
 use think\Db;
+use Config;
+use cache\Phpredis;
 
 class Recommend{
-
+   
     function delDataEmptyKey($data) {
         foreach ($data as $key => $value) {
             if (!$value) {
@@ -15,68 +17,183 @@ class Recommend{
         return $data;
     }
 
+    function SetRedis($data = []){
+        $this->redis = Phpredis::getConn();
+        $redisListKey = Config::get('redisKey.index.redisIndexShow');
+        $this->redis->set($redisListKey,json_encode($data));
+    }
     /**
      * 添加新记录
+     * @param $data
      * @return array
      * @author rzc
      */
-    public function addRecommend($data){
+    public function saveRecommend($data,$id = 0){
         $data = $this->delDataEmptyKey($data);
-        if ($data['tier'] > 1) {
-            if ($data['model_id']<8 && $data['model_id'] > 1) {
-                $model_num = [
-                    2 => 8,
-                    3 => 2,
-                    4 => 4,
-                    5 => 7,
-                    6 => 8,
-                    7 => 9,
-                ];
-                $has_num = DbRecommend::CountRecommends(['model_id'=>$data['model_id'],'tier'=>2]);
-                if ($has_num+1 > $model_num[$data['model_id']]) {
-                    return ['code' => '3009'];//超出限定添加数量
+        
+        if (!empty($id)){//更新操作
+            $recommend_info = DbRecommend::getRecommends('*',['id' => $id],true);
+            if (empty($recommend_info)) {
+                return ['code' => '3000'];
+            }
+            Db::startTrans();
+            try {
+                
+                $oldImage = $recommend_info['image_path'];
+                $oldImage = filtraImage(Config::get('qiniu.domain'), $oldImage);
+                if (!empty($oldImage)) {
+                    DbImage::updateLogImageStatus($oldImage, 3);//更新状态为已完成
                 }
-               
-            }elseif ($data['model_id'] == 10) {
-                if ($data['tier'] == 3){
-                    $has_num = DbRecommend::CountRecommends(['model_id'=>$data['model_id'],'tier'=>3]);
-                    if ($has_num+1 > 5) {
+                if (!empty($data['image_path'])) {
+                    $image    = filtraImage(Config::get('qiniu.domain'), $data['image_path']);
+                    $logImage = DbImage::getLogImage($image, 2);//判断时候有未完成的图片
+                    if (empty($logImage)) {//图片不存在
+                        return ['code' => '3010'];//图片没有上传过
+                    }
+                    DbImage::updateLogImageStatus($logImage, 1);//更新状态为已完成
+                }
+                $updateRecommends = DbRecommend::updateRecommends($data,$id);
+                $has_recommends = $this->getRecommendOrderBy();
+                if ($has_recommends['recommends']) {
+                    $this->SetRedis($has_recommends['recommends']);
+                }
+                if ($updateRecommends) {
+                    Db::commit();
+                    return ['code' => '200'];
+                }
+                Db::rollback();
+                return ['code' => '3011'];//修改失败
+            } catch (\Exception $e) {
+                Db::rollback();
+                return ['code' => '3011'];//修改失败
+            }
+        }else{//添加操作
+            if ($data['tier'] > 1) {
+                if ($data['model_id']<8 && $data['model_id'] > 1) {
+                    $model_num = [
+                        2 => 8,
+                        3 => 2,
+                        4 => 4,
+                        5 => 7,
+                        6 => 8,
+                        7 => 9,
+                    ];
+                    $has_num = DbRecommend::CountRecommends(['model_id'=>$data['model_id'],'tier'=>2]);
+                    if ($has_num+1 > $model_num[$data['model_id']]) {
                         return ['code' => '3009'];//超出限定添加数量
                     }
+                   
+                }elseif ($data['model_id'] == 10) {
+                    if ($data['tier'] == 3){
+                        $has_num = DbRecommend::CountRecommends(['model_id'=>$data['model_id'],'tier'=>3]);
+                        if ($has_num+1 > 5) {
+                            return ['code' => '3009'];//超出限定添加数量
+                        }
+                    }
+                }
+                
+            }else{
+                if (DbRecommend::getRecommends('id',['model_id'=>$data['model_id'],'tier'=>1])) {
+                    return ['code' => '3009'];//超出限定添加数量
                 }
             }
-            
-        }else{
-            if (DbRecommend::getRecommends('id',['model_id'=>$data['model_id'],'tier'=>1])) {
-                return ['code' => '3009'];//超出限定添加数量
+            Db::startTrans();
+            try {
+                
+                if (!empty($data['image_path'])) {
+                    $image    = filtraImage(Config::get('qiniu.domain'), $data['image_path']);
+                    $logImage = DbImage::getLogImage($image, 2);//判断时候有未完成的图片
+                    if (empty($logImage)) {//图片不存在
+                        return ['code' => '3010'];//图片没有上传过
+                    }
+                    DbImage::updateLogImageStatus($logImage, 1);//更新状态为已完成
+                }
+                $add = DbRecommend::addRecommends($data);
+                $has_recommends = $this->getRecommendOrderBy();
+                if ($has_recommends['recommends']) {
+                    $this->SetRedis($has_recommends['recommends']);
+                }
+                if ($add) {
+                    Db::commit();
+                    return ['code' => '200'];
+                }
+                Db::rollback();
+                return ['code' => '3011'];//修改失败
+            } catch (\Exception $e) {
+                Db::rollback();
+                return ['code' => '3011'];//修改失败
             }
         }
-        DbRecommend::addRecommends($data);
-        /* 写入首页缓存 */
-        return ['code' => 200];
-        
+  
+    
     }
     
     /**
-     * 查询推荐内容
+     * 查询推荐内容(已排序,倒序)
      * @return array
      * @author rzc
      */
-    public function getRecommend(){
+    public function getRecommendOrderBy(){
+        
         $recommends = [];
         $recommends = DbRecommend::getRecommends('id,model_id,title,image_path,jump_type,jump_content,model_order',['tier'=>1],false,'model_order','desc');
         if ($recommends) {
             foreach ($recommends as $key => $value) {
                 $recommends_son = DbRecommend::getRecommends('*',['tier'=>2,'parent_id' => $value['id']],false,'model_order','desc');
-                print_r($recommends_son);
+                if ($recommends_son) {
+                    if ($value['model_id'] == 10){
+                        foreach ($recommends_son as $recommend => $son) {
+                            $third = [];
+                            $third = DbRecommend::getRecommends('*',['tier'=>3,'parent_id' => $son['id']],false,'model_order','desc');
+                            $recommends_son[$recommend]['third'] = $third;
+                        }
+                    }
+                   
+                }
+                $recommends[$key]['son'] = $recommends_son;
+               
             }
+            // print_r($redisListKey);die;
+            
+            return ['code' => '200','recommends' => $recommends];
+        }else{
+            return ['code' => '3000'];
+        }
+    }
+
+    public function getRecommend(){
+        
+        $recommends = [];
+        $recommends_ids = [];
+        $recommends = DbRecommend::getRecommends('id,model_id,title,image_path,jump_type,jump_content,model_order',['tier'=>1],false,'model_id','asc');
+        if ($recommends) {
+            foreach ($recommends as $key => $value) {
+                $recommends_son = DbRecommend::getRecommends('*',['tier'=>2,'parent_id' => $value['id']],false,'id','asc');
+                if ($recommends_son) {
+                    if ($value['model_id'] == 10){
+                        foreach ($recommends_son as $recommend => $son) {
+                            $third = [];
+                            $third = DbRecommend::getRecommends('*',['tier'=>3,'parent_id' => $son['id']],false,'id','asc');
+                            $recommends_son[$recommend]['third'] = $third;
+                        }
+                    }
+                   
+                }
+                $recommends[$key]['son'] = $recommends_son;
+                $recommends_ids[] = $value['id'];
+            }
+            // print_r($redisListKey);die;
+            
+            return ['code' => '200','recommends_ids'=>$recommends_ids,'recommends' => $recommends];
         }else{
             return ['code' => '3000'];
         }
     }
 
     /**
-     * 查询推荐id 
+     * 查询推荐模板下的id集合 
+     * @param $model_id
+     * @param $tier
      * @return array
      * @author rzc
      */
@@ -102,4 +219,15 @@ class Recommend{
             }
         }
     }
+
+    public function getRecommendInfo($id){
+        $recommends = DbRecommend::getRecommends('*',['id' => $id],true);
+            if ($recommends) {
+                // $recommends_id = $recommends['id'];
+                return ['code' => '200','recommends_info' => $recommends];
+            }else{
+                return ['code' => '3000'];
+            }
+    }
+    
 }
