1f4c4bbd6211f27c39cfca7fa8ff71bce4d49494
wind-dust <1002380377@qq.com>
2019-03-06 19:49:26 +0800

    修改推荐is_show

--- application/admin/controller/Recommend.php
+++ application/admin/controller/Recommend.php
@@ -352,6 +352,7 @@ class Recommend extends AdminController
         $parent_id       = $parent_id ? $parent_id : 0;
         $tier            = $tier ? $tier : 1;
         $show_type       = $show_type ? $show_type : 1;
+        $is_show         = $is_show ? $is_show : 2;
         if (!is_numeric($model_id) && !is_numeric($id)) {
             return ['code' => '3001'];
         }
@@ -360,9 +361,7 @@ class Recommend extends AdminController
             return ['code' => '3002'];
         }
         
-        if ($tier != 1) {
-           unset($is_show);
-        }
+        
         if ($model_id == 1 || $model_id == 2 || $model_id == 3 ) {
             if ($tier == 2  ) {
                 if (!$title  || !$jump_type || !$jump_content){
@@ -445,7 +444,10 @@ class Recommend extends AdminController
         $data['tier'] = $tier;
         $data['model_order'] = $model_order;
         $data['model_son_order'] = $model_son_order;
-        $data['is_show'] = $is_show ? $is_show : 2;
+        $data['is_show'] = $is_show ;
+        if ($tier != 1) {
+            unset($data['is_show']);
+         }
         $result = $this->app->recommend->saveRecommend($data,$id);
         return $result;
     }
