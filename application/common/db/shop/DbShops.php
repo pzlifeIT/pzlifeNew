8b99405f948cb9c315cedd7ab2792c27cf2b3ca3
wind-dust <1002380377@qq.com>
2019-03-12 09:37:27 +0800

    店铺管理

--- application/common/db/shop/DbShops.php
+++ application/common/db/shop/DbShops.php
@@ -3,6 +3,7 @@
 namespace app\common\db\shop;
 
 use app\common\model\Shops;
+use app\common\model\ShopGoods;
 
 class DbShops {
     /**
@@ -17,5 +18,68 @@ class DbShops {
 
     public function getShops($where, $field) {
         return Shops::field($field)->where($where)->select()->toArray();
+    }
+
+    /**
+     * 获取店铺商品与商品表关联
+     * @param $where1 条件
+     * @param $where2 条件
+     * @param $field 字段
+     * @param $field2 字段
+     * @param $row 查多条还是一条
+     * @param $orderBy 排序字段
+     * @param $sc 排序方式
+     * @param $limit 查询分页
+     * @return array
+     * @author rzc
+     */
+    public function getShopGoodsWithGoods($where1,$where2,$field, $field2, $row = false, $orderBy = '', $sc = '', $limit = ''){
+        // $obj = ShopGoods::field($field)->where($where1);
+
+        $obj = ShopGoods::field($field)->withJoin(
+            ['Goods' => function ($query) use ($field2, $where2) {
+                $query->withField($field2)->where($where2);
+            },
+            ])->where($where1)->select()->toArray();
+
+        if (!empty($orderBy) && !empty($sc)) {
+            $obj = $obj->order($orderBy, $sc);
+        }
+        if (!empty($limit)) {
+            $obj = $obj->limit($limit);
+        }
+        if ($row === true) {
+            $obj = $obj->findOrEmpty();
+        } else {
+            $obj = $obj->select();
+        }
+        return $obj->toArray();
+    }
+
+    /**
+     * 获取店铺商品表信息
+     * @param $where 字段
+     * @param $field 条件
+     * @param $row 查多条还是一条
+     * @param $orderBy 排序字段
+     * @param $sc 排序方式
+     * @param $limit 查询分页
+     * @return array
+     * @author rzc
+     */
+    public function getShopGoods($where,$field,$row = false,$orderBy = '',$sc = '',$limit = ''){
+        $obj = ShopGoods::field($field)->where($where);
+        if (!empty($orderBy) && !empty($sc)) {
+            $obj = $obj->order($orderBy, $sc);
+        }
+        if (!empty($limit)) {
+            $obj = $obj->limit($limit);
+        }
+        if ($row === true) {
+            $obj = $obj->findOrEmpty();
+        } else {
+            $obj = $obj->select();
+        }
+        return $obj->toArray();
     }
 }
--- application/index/controller/ShopManage.php
+++ application/index/controller/ShopManage.php
@@ -3,7 +3,7 @@ namespace app\index\controller;
 //
 use app\index\MyController;
 //
-class ShopManage extends MyController{
+class Shopmanage extends MyController{
 
     protected $beforeActionList = [
        'isLogin',//所有方法的前置操作
@@ -15,20 +15,74 @@ class ShopManage extends MyController{
 
 
     /**
-     * @api              {post} / 处理推荐关系
+     * @api              {post} / 获取店铺商品
      * @apiDescription   getShopGoods
      * @apiGroup         index_user
      * @apiName          getShopGoods
      * @apiParam (入参) {String} con_id
-     * @apiParam (入参) {String} [buid] 推荐人uid
-     * @apiSuccess (返回) {String} code 200:成功  3001:con_id长度只能是32位 / 3002:conId有误
+     * @apiParam (入参) {String} type 查询类型 1:已上架 2:已下架 3:未上架
+     * @apiParam (入参) {String} search 查询字段
+     * @apiSuccess (返回) {String} code 200:成功  3001:con_id长度只能是32位 / 3002:conId有误 / 3003:type:查询类型必须为数字 / 3004:非法的查询类型 / 3005:店铺不存在
      * @apiSuccess (返回) {Array} data 用户信息
      * @apiSampleRequest /index/user/getShopGoods
      * @return array
-     * @author zyr
+     * @author rzc
      */
     public function getShopGoods(){
         $conId = trim($this->request->post('con_id'));
-        
+        $type = trim($this->request->post('type'));
+        $search = trim($this->request->post('search'));
+        $page = trim($this->request->post('page'));
+        $pagenum = trim($this->request->post('pagenum'));
+        $type = $type ? $type : 1;
+        $types = [1,2,3];
+        if (empty($conId)) {
+            return ['code' => '3002'];
+        }
+        if (strlen($conId) != 32) {
+            return ['code' => '3001'];
+        }
+        if (!is_numeric($type)) {
+            return ['code' => '3003'];
+        }
+        if (!in_array($type,$types)) {
+            return ['code' => '3004'];
+        }
+        $result = $this->app->shopmanage->getShopGoods($conId,$type,$search,$page,$pagenum);
+        return $result;
+    }
+
+    /**
+     * @api              {post} / 上下架店铺商品
+     * @apiDescription   autoShopGoods
+     * @apiGroup         index_user
+     * @apiName          autoShopGoods
+     * @apiParam (入参) {String} con_id
+     * @apiParam (入参) {String} type 查询类型 1:已上架 2:已下架 
+     * @apiSuccess (返回) {String} code 200:成功  3001:con_id长度只能是32位 / 3002:conId有误 / 3003:type:查询类型必须为数字 / 3004:非法的查询类型 / 3005:店铺不存在
+     * @apiSuccess (返回) {Array} data 用户信息
+     * @apiSampleRequest /index/user/autoShopGoods
+     * @return array
+     * @author rzc
+     */
+    public function autoShopGoods(){
+        $conId = trim($this->request->post('con_id'));
+        $type = trim($this->request->post('type'));
+        $type = $type ? $type : 1;
+        $types = [1,2,3];
+        if (empty($conId)) {
+            return ['code' => '3002'];
+        }
+        if (strlen($conId) != 32) {
+            return ['code' => '3001'];
+        }
+        if (!is_numeric($type)) {
+            return ['code' => '3003'];
+        }
+        if (!in_array($type,$types)) {
+            return ['code' => '3004'];
+        }
+        $result = $this->app->shopmanage->autoShopGoods($conId,$type);
+        return $result;
     }
 }
