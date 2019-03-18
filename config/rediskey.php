a1ea6828dbadaf144c0892c3b8773c362ea0799f
wind-dust <1002380377@qq.com>
2019-03-18 16:16:11 +0800

    订单增加物流流转信息

--- config/rediskey.php
+++ config/rediskey.php
@@ -4,9 +4,11 @@ return [
         'redisCartUserKey' => 'index:cart:user:',//用户购物车信息
     ],
     'order' => [
-        'redisOrderBonus'  => 'index:order:bonus:list:',//计算分利的订单队列的key(普通商品)
-        'redisMemberOrder' => 'index:member:order:list:',//购买会员成功支付的订单队列
-        'redisMemberShare' => 'index:member:share:list:',//购买会员成功支付的上级分享者获利提示队列
+        'redisOrderBonus'          => 'index:order:bonus:list:',//计算分利的订单队列的key(普通商品)
+        'redisMemberOrder'         => 'index:member:order:list:',//购买会员成功支付的订单队列
+        'redisMemberShare'         => 'index:member:share:list:',//购买会员成功支付的上级分享者获利提示队列
+        'redisDeliverOrderExpress' => 'cms:order:deliver:express:',//后台cms发货物流查询
+        'redisDeliverExpressList'  => 'cms:order:deliver:list:',//后台cms发货物流单号及物流公司编码队列
     ],
     'user'  => [
         'redisKey'           => 'index:user:',
