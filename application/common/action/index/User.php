8dd139f270d834bcd8b77bfddc726bb2db5d604e
zhangyanrong <cyre_zyr@163.com>
2019-04-11 12:01:20 +0800

    排序

--- application/common/action/index/User.php
+++ application/common/action/index/User.php
@@ -557,11 +557,11 @@ class User extends CommonIndex {
         }
 
         $field    = 'from_uid,to_uid,result_price,order_no,status,create_time';
-        $distinct = DbUser::getLogBonusDistinct($where, 'order_no', false, ['create_time' => 'desc'], $offset . ',' . $pageNum);
+        $distinct = DbUser::getLogBonusDistinct($where, 'order_no', false, 'order_no desc', $offset . ',' . $pageNum);
         $distinct = array_column($distinct, 'order_no');
         $where2   = $where;
         $where2[] = ['order_no', 'in', $distinct];
-        $bonus    = DbUser::getLogBonus($where2, $field);
+        $bonus    = DbUser::getLogBonus($where2, $field, false, 'create_time desc');
         $combined = DbUser::getLogBonusSum($where, 'result_price'); //合计
         $userList = DbUser::getUserInfo([['id', 'in', array_unique(array_column($bonus, 'from_uid'))]], 'id,nick_name,avatar,user_identity');
         $userList = array_combine(array_column($userList, 'id'), $userList);
