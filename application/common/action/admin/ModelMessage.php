6c639ba88603a0cb56c8beef28be865b9ff43a6d
wind-dust <1002380377@qq.com>
2019-05-21 15:59:52 +0800

    修正模板消息

--- application/common/action/admin/ModelMessage.php
+++ application/common/action/admin/ModelMessage.php
@@ -374,7 +374,7 @@ class ModelMessage extends CommonIndex {
         if (empty($message_template)) {
             return ['code' => '3004'];
         }
-        $trigger = DbModelMessage::getMessageTemplate(['id' => $trigger_id, 'status' => 2], '*', true);
+        $trigger = DbModelMessage::getTrigger(['id' => $trigger_id, 'status' => 2], '*', true);
         if (empty($trigger)) {
             return ['code' => '3005'];
         }
