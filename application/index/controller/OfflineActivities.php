c1c244d341b6ce29381c0bf26fa8a5b77f9bd68d
wind-dust <1002380377@qq.com>
2019-05-29 17:59:37 +0800

    修正use文件

---
+++ application/index/controller/OfflineActivities.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+//
+use app\index\MyController;
+//
+class OfflineActivities extends MyController{
+//
+//    public function getCollectGoods(){
+//
+//        $res = $this->app->collect->collectGoods($user_id);
+//        return $res;
+//    }
+}
