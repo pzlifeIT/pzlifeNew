c5c8ecfcd54cc78d8e54754a430e765668ac569d
wind-dust <1002380377@qq.com>
2019-06-21 16:00:01 +0800

    修正查询接口

--- application/common/action/supadmin/Promote.php
+++ application/common/action/supadmin/Promote.php
@@ -22,11 +22,20 @@ class Promote extends CommonIndex {
      * @return array
      * @author zyr
      */
-    public function getSupPromoteSignUp($promote_id, $page, $pageNum, $nick_name = '', $mobile = '', $start_time = '',$end_time = '') {
+    public function getSupPromoteSignUp($promote_id, $page, $pageNum, $nick_name = '', $mobile = '', $start_time = '', $end_time = '') {
         $where = [];
         array_push($where, [['id', '=', $promote_id]]);
+
+        $promote = DbSup::getSupPromote($where, 'id', true);
+        if (empty($promote)) {
+            return ['code' => '3002']; //推广活动不存在
+        }
+        $offset = ($page - 1) * $pageNum;
+        if ($offset < 0) {
+            return ['code' => '200', 'suppromotesignup' => []];
+        }
         if (!empty($nick_name)) {
-            array_push($where, [['nick_name', 'LIKE', '%'.$nick_name.'%']]);
+            array_push($where, [['nick_name', 'LIKE', '%' . $nick_name . '%']]);
         }
         if (!empty($mobile)) {
             array_push($where, [['mobile', '=', $mobile]]);
@@ -37,16 +46,8 @@ class Promote extends CommonIndex {
         if (!empty($end_time)) {
             array_push($where, [['create_time', '<=', $end_time]]);
         }
-        $promote = DbSup::getSupPromote($where, 'id', true);
-        if (empty($promote)) {
-            return ['code' => '3002']; //推广活动不存在
-        }
-        $offset = ($page - 1) * $pageNum;
-        if ($offset < 0) {
-            return ['code' => '200', 'suppromotesignup' => []];
-        }
-        $result = DbSup::getSupPromoteSignUp(['promote_id' => $promote_id], 'id,nick_name,mobile,create_time', false, ['create_time' => 'ASC'],$offset.','.$pageNum);
-        $total = DbSup::getSupPromoteSignUpCount($where);
+        $result = DbSup::getSupPromoteSignUp($where, 'id,nick_name,mobile,create_time', false, ['create_time' => 'ASC'], $offset . ',' . $pageNum);
+        $total  = DbSup::getSupPromoteSignUpCount($where);
         return ['code' => '200', 'suppromotesignup' => $result];
     }
 
