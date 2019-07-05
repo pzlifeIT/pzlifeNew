e9fabe75a8b80ac8dc5f074e9177310f18eda779
wind-dust <1002380377@qq.com>
2019-07-05 12:48:35 +0800

    获取微信文章

--- application/console/com/WeChatGraphicMaterial.php
+++ application/console/com/WeChatGraphicMaterial.php
@@ -69,6 +69,9 @@ class WeChatGraphicMaterial extends Pzlife {
             
         } while (!$requsest_subject);
        
+        if (empty($news)) {
+            exit('news_is_null');
+        }
        foreach ($news as $n => $new) {
             $this->redis->Zadd($redisBatchgetMaterial,$n,json_encode($new));
        }
@@ -104,9 +107,9 @@ class WeChatGraphicMaterial extends Pzlife {
         $access_token = $this->redis->get($redisAccessTokenTencent);
         if (empty($access_token)) {
             $appid = Env::get('weixin.weixin_appid');
-            $appid         = 'wx112088ff7b4ab5f3';
+            // $appid         = 'wx112088ff7b4ab5f3';
             $secret = Env::get('weixin.weixin_secret');
-            $secret        = 'db7915c4a840421683be99c6d798757f';
+            // $secret        = 'db7915c4a840421683be99c6d798757f';
             $requestUrl = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' . $appid . '&secret=' . $secret;
             $requsest_subject = json_decode(sendRequest($requestUrl), true);
             if (!isset($requsest_subject['access_token'])) {
@@ -114,8 +117,8 @@ class WeChatGraphicMaterial extends Pzlife {
             }
             $access_token     = $requsest_subject['access_token'];
             
-            // $this->redis->set($redisAccessTokenTencent,$access_token);
-            // $this->redis->expire($redisAccessTokenTencent, 6600);
+            $this->redis->set($redisAccessTokenTencent,$access_token);
+            $this->redis->expire($redisAccessTokenTencent, 6600);
         }
         
         return $access_token;
