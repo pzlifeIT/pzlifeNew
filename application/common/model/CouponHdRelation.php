8c7c71b3893bda7eb2c95671b3eaf1edd033dbf2
zhangyanrong <cyre_zyr@163.com>
2019-07-17 11:28:03 +0800

    coupon fix

--- application/common/model/CouponHdRelation.php
+++ application/common/model/CouponHdRelation.php
@@ -2,17 +2,14 @@
 
 namespace app\common\model;
 
-use think\model\concern\SoftDelete;
 use think\model\Pivot;
 
 class CouponHdRelation extends Pivot {
-    use SoftDelete;
     protected $pk = 'id';
     // 设置当前模型对应的完整数据表名称
     protected $table = 'pz_coupon_hd_relation';
     // 设置当前模型的数据库连接
     protected $connection = '';
-    protected $deleteTime = 'delete_time';
     protected $defaultSoftDelete = 0;
     protected $autoWriteTimestamp = true;
     protected $updateTime = false;//关闭update_time
