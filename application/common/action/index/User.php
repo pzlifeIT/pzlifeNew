3ee29d97084c40fe34d9c7792cf55b65c0466a16
wind-dust <1002380377@qq.com>
2019-07-30 16:58:45 +0800

    接口变更

--- application/common/action/index/User.php
+++ application/common/action/index/User.php
@@ -911,7 +911,7 @@ class User extends CommonIndex {
         if (empty($user)) {
             return ['code' => '3003'];
         }
-        if ($user['user_identity'] != '4') {
+        if ($user['user_identity'] < '2') {
             return ['code' => '3000']; //boss才有权限查看
         }
         $offset = ($page - 1) * $pageNum;
