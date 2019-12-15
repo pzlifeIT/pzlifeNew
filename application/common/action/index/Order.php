a947b3b871ee67220aa32b42e869c5d4f1720c31
Wind-dust <1002380377@qq.com>
2019-12-15 16:53:14 +0800

    错误抛出

--- application/common/action/index/Order.php
+++ application/common/action/index/Order.php
@@ -2531,6 +2531,7 @@ class Order extends CommonIndex {
             Db::commit();
             return ['code' => '200', 'order_no' => $orderNo, 'is_pay' => $isPay ? 1 : 2];
         } catch (\Exception $e) {
+            exception($e);
             Db::rollback();
             return ['code' => '3009'];
         }
