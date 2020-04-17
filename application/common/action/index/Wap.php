87227714854d73448f1cf2418f92e9aa4ec6709d
Wind-dust <1002380377@qq.com>
2020-04-18 02:16:20 +0800

    接口修正

--- application/common/action/index/Wap.php
+++ application/common/action/index/Wap.php
@@ -293,7 +293,7 @@ class Wap extends CommonIndex
             return ['code' => '3002'];
         }
         foreach ($project_id as $key => $value) {
-            $card = DbAdmin::getSamplingCard(['id' => $value, 'status' => 1], '*', true);
+            $card = DbAdmin::getSamplingReport(['id' => $value, 'status' => 1], '*', true);
             if (empty($card)) {
                 return ['code' => '3003', 'msg' => '存在未核验激活项目卡'];
             }
@@ -339,7 +339,7 @@ class Wap extends CommonIndex
             return ['code' => '3002'];
         }
         foreach ($project_id as $key => $value) {
-            $card = DbAdmin::getSamplingCard(['id' => $value, 'status' => 1], '*', true);
+            $card = DbAdmin::getSamplingReport(['id' => $value, 'status' => 1], '*', true);
             if (empty($card)) {
                 return ['code' => '3003', 'msg' => '存在未激活或已使用项目卡'];
             }
