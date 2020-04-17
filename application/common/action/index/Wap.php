02aea6073cc69b24209ecbd6fa62f2efb928c808
Wind-dust <1002380377@qq.com>
2020-04-17 23:40:54 +0800

    代码更新

--- application/common/action/index/Wap.php
+++ application/common/action/index/Wap.php
@@ -400,4 +400,33 @@ class Wap extends CommonIndex
         $result['projects'] = join(',', $sampling_data);
         return ['code' => '200', 'result' => $result];
     }
+
+    /**
+     * 通过省id获取省下面的所有市
+     * @param int $pid 上级id
+     * @param int $level 层级 1.省 2.市 3.区
+     * @return array
+     * @author zyr
+     */
+    public function getArea($pid, $level)
+    {
+        $field    = 'id,area_name,pid';
+        $where    = [
+            'id'    => $pid,
+            'level' => $level - 1,
+        ];
+        $province = DbProvinces::getAreaInfo($field, $where);
+        if (empty($province)) { //判断省市是否存在
+            return ['code' => '3001'];
+        }
+        $where2 = [
+            'pid'   => $pid,
+            'level' => $level,
+        ];
+        $result = DbProvinces::getAreaInfo($field, $where2);
+        if (empty($result)) { //获取下级列表
+            return ['code' => '3000'];
+        }
+        return ['code' => '200', 'data' => $result];
+    }
 }
