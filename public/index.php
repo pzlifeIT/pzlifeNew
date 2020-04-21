31a5ce99ed8287f1b3d3d22ea07d52c19508d68d
wind-dust <1002380377@qq.com>
2020-04-21 14:55:10 +0800

    跨域解决

--- public/index.php
+++ public/index.php
@@ -16,6 +16,6 @@ namespace think;
 require __DIR__ . '/../thinkphp/base.php';
 
 // 支持事先使用静态方法设置Request对象和Config对象
-
+header('Access-Control-Allow-Headers: content-type,token');
 // 执行应用并响应
 Container::get('app')->run()->send();
