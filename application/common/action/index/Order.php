3d75818f4b62b0b0fa16240bb3966eccfc5a2c54
wind-dust <1002380377@qq.com>
2020-07-03 03:39:04 +0800

    代码调试

--- application/common/action/index/Order.php
+++ application/common/action/index/Order.php
@@ -1032,6 +1032,7 @@ class Order extends CommonIndex {
         }
         $prefix   = $this->prefix;
         $carts    = $this->redis->hKeys($this->redisCartUserKey . $uid);
+        print_r($carts);die;
         $cartList = array_map(function ($v) use ($prefix) {
             return str_replace($prefix, '', $v);
         }, $carts);
