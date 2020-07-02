49b95bf671f443f181403e6d5a9091c546fa7413
wind-dust <1002380377@qq.com>
2020-07-03 04:44:10 +0800

    修正

--- application/common/action/index/Order.php
+++ application/common/action/index/Order.php
@@ -1032,7 +1032,7 @@ class Order extends CommonIndex {
         }
         $prefix   = $this->prefix;
         $carts    = $this->redis->hKeys($this->redisCartUserKey . $uid);
-        print_r($this->redis->hKeys($this->redisCartUserKey . $uid));die;
+        // print_r($this->redis->hKeys($this->redisCartUserKey . $uid));die;
         $cartList = array_map(function ($v) use ($prefix) {
             return str_replace($prefix, '', $v);
         }, $carts);
